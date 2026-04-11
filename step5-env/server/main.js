27f9458dbe8433fc584f3c4ffea44442298b2fb1
fernando <413114463@qq.com>
2026-04-11 19:26:20 +0800

    加入mysql到compose中

--- step5-env/server/main.js
+++ step5-env/server/main.js
@@ -1,14 +1,40 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
+const mysql = require("mysql2/promise");
+
+// console.log(connection, connection.connect);
+// .connect()
+// .then(() => {
+//   console.log("Connected to MySQL");
+// })
+// .catch((err) => {
+//   console.error("Error connecting to MySQL", err);
+// });
 
 // 加载环境变量
 dotenv.config();
 
+const connection = mysql.createConnection({
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+});
+
+connection
+  .then(() => {
+    console.log("Connected to MySQL successfully");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MySQL", err);
+  });
+
 const port = 4000;
 
-app.get('/echo', (req, res) => {
-  res.send('Hello from step5! Database host: ' + process.env.DATABASE_NAME);
+app.get("/echo", (req, res) => {
+  res.send("Hello from step5! Database host: " + process.env.DATABASE_NAME);
 });
 
 app.listen(port, () => {
