4e3bcaec8fabd05f81d7e2f796af8a841db6c41b
fernando <413114463@qq.com>
2026-04-13 13:00:02 +0800

    加test启动脚本

--- step9-share443/start-test.sh
+++ step9-share443/start-test.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker compose -f docker-compose.test.yml --env-file ./server/.env.prod up -d --build
+docker compose -f docker-compose.test.yml --env-file ./server/.env.test up -d --build
