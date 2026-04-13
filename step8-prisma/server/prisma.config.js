16c36430f3b6c96b185d174acceca2ac70bf64a2
fernando <413114463@qq.com>
2026-04-13 10:18:46 +0800

    加seed文件

--- step8-prisma/server/prisma.config.js
+++ step8-prisma/server/prisma.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
+        seed: "node --import ./prisma/seed-env-preload.js ./prisma/seed.js",
   },
   datasource: {
     url: process.env["DATABASE_URL"],
